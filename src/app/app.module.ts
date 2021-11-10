1d86339515fc7ba831dc4b13e380a549802f33d0
@mae-lab-hub <asmaeallou@outlook.com>
2021-11-09 23:06:30 -0500

    Added the basic pages components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './partials/header/header.component';
+import { FooterComponent } from './partials/footer/footer.component';
+import { BasepageComponent } from './partials/basepage/basepage.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ListComponent } from './pages/survey/list/list.component';
+import { CreateComponent } from './pages/survey/create/create.component';
+import { UpdateComponent } from './pages/survey/update/update.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    BasepageComponent,
+    HomeComponent,
+    ListComponent,
+    CreateComponent,
+    UpdateComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/partials/basepage/basepage.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-basepage',
+  templateUrl: './basepage.component.html',
+  styleUrls: ['./basepage.component.css']
+})
+export class BasepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
