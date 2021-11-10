195bac101777acf3eda74e6dce7a5d10e247490f
@mae-lab-hub <asmaeallou@outlook.com>
2021-11-10 00:03:28 -0500

    Added the logic to add the title.

--- src/app/partials/basepage/basepage.component.ts
+++ src/app/partials/basepage/basepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-basepage',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BasepageComponent implements OnInit {
 
-  constructor() { }
+  title: string;
+  constructor(private route: ActivatedRoute) { this.title =''; }
 
   ngOnInit(): void {
+    this.title = this.route.snapshot.data['title'];
   }
 
 }
