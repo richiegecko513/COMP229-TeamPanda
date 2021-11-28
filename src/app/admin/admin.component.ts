6815f376c69b4341facb773243561b7ce99fd11a
@mae-lab-hub <asmaeallou@outlook.com>
2021-11-28 06:09:35 -0500

    added admin component.

---
+++ src/app/admin/admin.component.ts
@@ -0,0 +1,18 @@
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
+import { AuthService } from "../model-2/auth.service";
+
+@Component({
+
+    templateUrl: './admin.component.html'
+})
+export class AdminComponent{
+
+    constructor(private auth: AuthService,
+                private router: Router){}
+
+    logout():void{
+        this.auth.logout();
+        this.router.navigateByUrl('/');
+    }
+}
