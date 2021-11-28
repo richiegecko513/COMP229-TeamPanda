3e1faee5041e245666507db23b1c0ac37963a799
@mae-lab-hub <asmaeallou@outlook.com>
2021-11-28 02:54:09 -0500

    Added jwtHelperService + JWT_OPTIONS to providers

--- src/app/model-2/model.module.ts
+++ src/app/model-2/model.module.ts
@@ -1,5 +1,7 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
+import { JWT_OPTIONS, JwtHelperService } from "@auth0/angular-jwt";
+import { AuthService } from "./auth.service";
 import { RestDataSource } from "./rest.datasource";
 import { StaticDataSource } from "./static.datasource";
 import { SurveyRepository } from "./survey.repository";
@@ -7,6 +9,9 @@ import { SurveyRepository } from "./survey.repository";
 
 @NgModule({
     imports: [HttpClientModule],
-    providers: [SurveyRepository, StaticDataSource, {provide: StaticDataSource, useClass: RestDataSource }, RestDataSource],
+    providers: [SurveyRepository, StaticDataSource, {provide: StaticDataSource, useClass: RestDataSource }, RestDataSource, AuthService,{
+        provide: JWT_OPTIONS,
+        useValue: JWT_OPTIONS
+     }, JwtHelperService],
 })
 export class SurveyModule{}
