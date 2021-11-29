b62b13cc0d5a867e6c9ab5e9d77cce26e70d4a90
@mae-lab-hub <asmaeallou@outlook.com>
2021-11-28 23:15:03 -0500

    Added Update routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { CreateComponent } from './pages/survey/create/create.component';
 import { ListComponent } from './pages/survey/list/list.component';
 import { LoginComponent } from './pages/login/login.component';
+import { UpdateComponent } from './pages/survey/update/update.component';
 
 
 
@@ -14,8 +15,9 @@ const routes: Routes = [
   {path: 'survey-list', component: ListComponent, data:{title:'Surveys'}},
   {path: 'login', component: LoginComponent, data:{title:'Login/Register'}},
   {path: 'login2', data:{title:'Login'}, redirectTo:'/admin/auth' ,pathMatch:'full'},
-  {path: 'admin', loadChildren:()=> import('./admin/admin.module').then(m=>m.AdminModule)}
-
+  {path: 'admin', loadChildren:()=> import('./admin/admin.module').then(m=>m.AdminModule)},
+  {path: ':mode/:id', component: UpdateComponent, data:{title: ""}},
+  {path: ':mode/', component: UpdateComponent, data:{title: "Create Survey"}}
 ];
 
 @NgModule({
