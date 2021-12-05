888d6cfd29f71edecf5dac032c6306fcd6ac464c
@mae-lab-hub <asmaeallou@outlook.com>
2021-12-05 04:08:05 -0500

    Added fill component to path.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { CreateComponent } from './pages/survey/create/create.component';
 import { ListComponent } from './pages/survey/list/list.component';
 import { LoginComponent } from './pages/login/login.component';
 import { UpdateComponent } from './pages/survey/update/update.component';
+import { FillComponent } from './pages/survey/fill/fill.component';
 
 
 
@@ -17,7 +18,8 @@ const routes: Routes = [
   {path: 'login2', data:{title:'Login'}, redirectTo:'/admin/auth' ,pathMatch:'full'},
   {path: 'admin', loadChildren:()=> import('./admin/admin.module').then(m=>m.AdminModule)},
   {path: ':mode/:id', component: UpdateComponent, data:{title: ""}},
-  {path: ':mode/', component: UpdateComponent, data:{title: "Create Survey"}}
+  {path: ':mode/', component: UpdateComponent, data:{title: "Create Survey"}},
+  {path:'survey-list/fill/:id', component: FillComponent}
 ];
 
 @NgModule({
