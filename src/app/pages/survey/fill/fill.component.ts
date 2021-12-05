1818ce5aec442bb8d6499631e40d928698f89114
@mae-lab-hub <asmaeallou@outlook.com>
2021-12-05 04:50:39 -0500

    Added method to save form.

--- src/app/pages/survey/fill/fill.component.ts
+++ src/app/pages/survey/fill/fill.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Survey } from 'src/app/model-2/survey.model';
 import { SurveyRepository } from 'src/app/model-2/survey.repository';
 import { ActivatedRoute, Router } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { SurveyResponse } from 'src/app/model-2/survey-response.model';
 
 @Component({
   selector: 'app-fill',
@@ -12,8 +14,10 @@ export class FillComponent implements OnInit {
 
   survey: Survey = new Survey();
 
+  response: SurveyResponse = new SurveyResponse();
+
   constructor(private repository: SurveyRepository,
-              private activeRoute: ActivatedRoute) { 
+              private activeRoute: ActivatedRoute,  private router: Router) { 
               
       Object.assign(this.survey, repository.getSurvey(activeRoute.snapshot.params.id));  
   
@@ -24,6 +28,14 @@ export class FillComponent implements OnInit {
    
     console.log(this.survey);
     console.log(this.activeRoute.snapshot.params.id);
+  }
+
+  save(form: NgForm): void{
+
+    this.repository.saveResponse(this.response);
+    //SAVE response
+    this.router.navigateByUrl('/survey-list')
+
   }
 
 
