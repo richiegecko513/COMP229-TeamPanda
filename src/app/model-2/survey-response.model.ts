7435afea9ef3d31c8bdd265293182db25f301a29
@mae-lab-hub <asmaeallou@outlook.com>
2021-12-05 04:48:46 -0500

    Created response model fron-end.

---
+++ src/app/model-2/survey-response.model.ts
@@ -0,0 +1,22 @@
+export class SurveyResponse{
+
+    constructor(
+        public _id? : number,
+        public title?: string,
+        public q1?: string,
+        public q2?: string,
+        public q3?: string,
+        public q4?: string,
+        public q5?: string,
+        public q6?: string,
+        public q7?: string,
+        public q8?: string,
+        public q9?: string,
+        public q10?: string, 
+    
+    ){}
+
+
+
+
+}
