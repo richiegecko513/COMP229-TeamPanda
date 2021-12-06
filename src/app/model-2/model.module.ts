52f9337cbd59b92ee2ef3d0aaa17af75fea50a07
@mae-lab-hub <asmaeallou@outlook.com>
2021-12-06 00:45:41 -0500

    Removed  jwtHelperService.

--- src/app/model-2/model.module.ts
+++ src/app/model-2/model.module.ts
@@ -1,17 +1,14 @@
 import { HttpClientModule } from "@angular/common/http";
 import { NgModule } from "@angular/core";
-import { JWT_OPTIONS, JwtHelperService } from "@auth0/angular-jwt";
 import { AuthService } from "./auth.service";
 import { RestDataSource } from "./rest.datasource";
 import { StaticDataSource } from "./static.datasource";
 import { SurveyRepository } from "./survey.repository";
-
-
-@NgModule({
+@NgModule( {
     imports: [HttpClientModule],
-    providers: [SurveyRepository, StaticDataSource, {provide: StaticDataSource, useClass: RestDataSource }, RestDataSource, AuthService,{
-        provide: JWT_OPTIONS,
-        useValue: JWT_OPTIONS
-     }, JwtHelperService],
+    providers: [SurveyRepository, StaticDataSource, 
+      {provide: StaticDataSource, useClass: RestDataSource},
+    RestDataSource, AuthService]
 })
 export class SurveyModule{}
+
