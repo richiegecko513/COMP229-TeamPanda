d3a4ac4435b82828d99a52196bf86f30873a4f9a
@mae-lab-hub <asmaeallou@outlook.com>
2021-12-06 02:01:49 -0500

    Changed login routing and addeed surveyguard.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,16 +6,17 @@ import { ListComponent } from './pages/survey/list/list.component';
 import { LoginComponent } from './pages/login/login.component';
 import { UpdateComponent } from './pages/survey/update/update.component';
 import { FillComponent } from './pages/survey/fill/fill.component';
+import { SurveyGuard } from './guards/survey.guard';
 
 
 
 const routes: Routes = [
   {path:'home', component: HomeComponent, data:{title:'Home'}},
   {path:'', redirectTo:'/home', pathMatch:'full'},
-  {path:'create', component: CreateComponent, data:{title:'Create Survey'}},
+  //{path:'create', component: CreateComponent, data:{title:'Create Survey'}},
   {path: 'survey-list', component: ListComponent, data:{title:'Surveys'}},
-  {path: 'login', component: LoginComponent, data:{title:'Login/Register'}},
-  {path: 'login2', data:{title:'Login'}, redirectTo:'/admin/auth' ,pathMatch:'full'},
+  //{path: 'login', component: LoginComponent, data:{title:'Login/Register'}},
+  {path: 'login', data:{title:'Login'}, redirectTo:'/admin/auth' ,pathMatch:'full'},
   {path: 'admin', loadChildren:()=> import('./admin/admin.module').then(m=>m.AdminModule)},
   {path: ':mode/:id', component: UpdateComponent, data:{title: ""}},
   {path: ':mode/', component: UpdateComponent, data:{title: "Create Survey"}},
@@ -24,6 +25,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers:[SurveyGuard]
 })
 export class AppRoutingModule { }
