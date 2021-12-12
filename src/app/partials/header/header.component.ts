057ac5a07fbb2645cf0055e03c2aa09a6abb41f9
Lance Nelson <lance.nelson@hotmail.com>
2021-12-12 17:41:40 -0500

    Logout button and redirect when logging out

--- src/app/partials/header/header.component.ts
+++ src/app/partials/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from 'src/app/model-2/auth.service';
+import { User } from 'src/app/model-2/user.model';
 
 @Component({
   selector: 'app-header',
@@ -8,13 +10,36 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
 
-  constructor(private router:Router) { }
+  user: User
+  
+  constructor(private authService: AuthService,
+              private router:Router) { }
 
- // ngOnInit(): void {
-  //}
+ ngOnInit(): void {
+  
+  this.user = new User();
+
+  }
   createSurvey():void{
 
     this.router.navigateByUrl('/survey-list/create')
 
+  }
+
+  isLoggedIn(): boolean
+  {
+    const result = this.authService.authenticated;
+    if (result)
+    {
+      this.user = JSON.parse(localStorage.getItem('user'));
+    }
+    return result;
+  }
+
+  onLogoutClick(): void
+  {
+    this.authService.logout().subscribe(data => {
+      this.router.navigate(['/login']);
+    });
   }
 }
