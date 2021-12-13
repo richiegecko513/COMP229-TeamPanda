0cbcb77b28f3571fcac85ef29d0e234e12842ad2
@mae-lab-hub <asmaeallou@outlook.com>
2021-12-12 20:18:02 -0500

    Cleaned up the code.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { CreateComponent } from './pages/survey/create/create.component';
 import { ListComponent } from './pages/survey/list/list.component';
-import { LoginComponent } from './pages/login/login.component';
-import { UpdateComponent } from './pages/survey/update/update.component';
 import { FillComponent } from './pages/survey/fill/fill.component';
 import { SurveyGuard } from './guards/survey.guard';
-
-
 
 const routes: Routes = [
   {path:'home', component: HomeComponent, data:{title:'Home'}},
   {path:'', redirectTo:'/home', pathMatch:'full'},
-  //{path:'create', component: CreateComponent, data:{title:'Create Survey'}},
+
   {path: 'survey-list', component: ListComponent, data:{title:'Surveys'}},
-  //{path: 'login', component: LoginComponent, data:{title:'Login/Register'}},
+
   {path: 'login', data:{title:'Login'}, redirectTo:'/admin/auth' ,pathMatch:'full'},
   {path: 'admin', loadChildren:()=> import('./admin/admin.module').then(m=>m.AdminModule)},
-  {path: ':mode/:id', component: UpdateComponent, data:{title: ""}},
-  {path: ':mode/', component: UpdateComponent, data:{title: "Create Survey"}},
+
   {path:'survey-list/fill/:id', component: FillComponent}
 ];
 
