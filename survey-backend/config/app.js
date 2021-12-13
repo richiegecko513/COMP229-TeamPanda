14225facfa73830fb0caef1dc434e0a883f4507d
@mae-lab-hub <asmaeallou@outlook.com>
2021-12-12 22:15:26 -0500

    Commented out port for deployment.

--- survey-backend/config/app.js
+++ survey-backend/config/app.js
@@ -102,12 +102,12 @@ app.get("*",(req, res)=>{
 });
 
 //PORT
-const port = process.env.PORT || 3000;
+//const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
+/*app.listen(port, () => {
   console.log('Listening on port ' + port)
 })
-
+*/
 /*
 // catch 404 and forward to error handler
  app.use(function(req, res, next) {
